8e8ca238f87630665ba6540f0b6a3a8cd414bf27
Mathtician <apourkav@andrew.cmu.edu>
2021-10-14 20:22:27 -0400

    fixed bug

--- RRarduino/RRarduino.ino
+++ RRarduino/RRarduino.ino
@@ -55,10 +55,18 @@ void loop() {
     float vp1 = expo + expo2;
     float vp2 = expo - expo2;
 
-    float scaleCorrect = 1/max(abs(vp1), abs(vp2));
+    float avp1 = abs(vp1);
+    float avp2 = abs(vp2);
 
-    v1 = vp1*scaleCorrect;
-    v2 = vp2*scaleCorrect;
+    if (avp1 <= 1 && avp2 <= 1) {
+      scaleCorrect = 1;
+    }
+    else {
+      scaleCorrect = 1 / max(avp1, avp2);
+    }
+
+    v1 = vp1 * scaleCorrect;
+    v2 = vp2 * scaleCorrect;
   }
 
   setVelocity(motor1, v1 * max_speed);
